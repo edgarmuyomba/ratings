274eca91f66b2988baebc03402a9ac484aa88b82
edgarmuyomba <edgarmatthew247@gmail.com>
2023-09-27 19:48:45 +0300

    fix: django secret key

--- ratings/settings.py
+++ ratings/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*10^h2kmmg2a!opp@03aub+^i4fwu7-9fs2hyhvop(#^)0#!6t'
+SECRET_KEY = os.getenv("DJANGO_SECRETKEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
