760b458597b69ca46f40d080c555f976a7e2a95d
edgarmuyomba <edgarmatthew247@gmail.com>
2023-09-28 22:35:50 +0300

    feature: static files

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from django.views.generic import TemplateView
 from .views import search, details, episode_ratings
+from django.conf.urls.static import static
+from ratings.settings import STATIC_URL, STATIC_ROOT
 
 app_name = 'core'
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('search/', search, name="search"),
     path('details/<str:imdbId>/<str:type>/', details, name="details"),
     path('details/episode_ratings/<str:imdbID>/', episode_ratings, name="episode_ratings"),
-]
+] + static(STATIC_URL, document_root=STATIC_ROOT)
