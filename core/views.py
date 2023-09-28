f6cfdcccb55ff211791e1e9b74547d3618a548ce
edgarmuyomba <edgarmatthew247@gmail.com>
2023-09-28 21:46:31 +0300

    feature: scrape episodes

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from dotenv import load_dotenv
 import os
 import requests
+import time
 from bs4 import BeautifulSoup
 from requests import Session
 
@@ -40,7 +41,7 @@ def details(request, imdbId, type):
         # web scraping
         data = runScraper(imdbId)
         context = data
-        poster = requests.get(f"https://img.omdbapi.com/?apikey={omdbKey}&i={imdbId}")
+        poster = requests.get(f"https://www.omdbapi.com/?apikey={omdbKey}&i={imdbId}").json()['Poster']
         context['poster'] = poster
         return render(request, "core/serie_details.html", context)
 
@@ -65,6 +66,23 @@ def runScraper(imdbID):
     title = bs.find("span", {"class": "sc-afe43def-1"}).text
     plot = bs.find("span", {"class": "sc-466bb6c-0 kJJttH"}).text
     rating = bs.find("span", {"class": "sc-bde20123-1 iZlgcd"}).text
+    time.sleep(1)
+    res2 = session.get(f"https://www.imdb.com/title/{imdbID}/episodes/", headers=headers)
+    bs2 = BeautifulSoup(res2.text, "html.parser")
+    seasons = bs2.find_all("li", {"class": "ipc-tab ipc-tab--on-base"})
+    for season in seasons:
+        try:
+            season = int(season.text)
+        except:
+            # value isnt an integer
+            print(season)
+            pass 
+        else:
+            time.sleep(1)
+            episode_page = session.get(f"https://www.imdb.com/title/{imdbID}/episodes/?season={season}", headers=headers)
+            bs3 = BeautifulSoup(episode_page.text, "html.parser")
+            episodes = bs3.find_all("article", {"class": "sc-f1a948e3-1 bGxjcH episode-item-wrapper"})
+            print(len(episodes))
     return {
         "title": title,
         "plot": plot,
