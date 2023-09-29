982a22fb6f11f01feebd397daf4ed5633eded8fe
edgarmuyomba <edgarmatthew247@gmail.com>
2023-09-29 21:26:03 +0300

    features: js tablestyles

--- core/static/core/js/details.js
+++ core/static/core/js/details.js
@@ -13,25 +13,26 @@ async function get_ratings(imdbId) {
 function displayRatings(ratings) {
     let last_ep = 1;
     let header = document.createElement('tr');
-    let _ = document.createElement('td');
+    let _ = document.createElement('th');
     header.appendChild(_);
     for (let season in ratings) {
         last_ep = Math.max(last_ep, ratings[season].length);
-        let head = document.createElement('td');
+        let head = document.createElement('th');
         head.textContent = season;
         header.appendChild(head);
     }
     episodes.appendChild(header);
-    
+
     for (let i = 1; i <= last_ep; i++) {
         let row = document.createElement('tr');
         let season = document.createElement('td');
         season.textContent = i;
+        season.classList.add("index");
         row.appendChild(season);
 
         for (let season in ratings) {
             let rate = document.createElement('td');
-            rate.textContent = ratings[season][i-1] ? ratings[season][i-1] : '';
+            rate.textContent = ratings[season][i - 1] ? ratings[season][i - 1] : '';
             row.appendChild(rate);
         }
         episodes.appendChild(row);
@@ -39,13 +40,38 @@ function displayRatings(ratings) {
 }
 
 (async () => {
-    // const ratings = await get_ratings(id);
+    const ratings = await get_ratings(id);
     const rates = {
-        1: [1, 2, 3, 4, 5],
-        2: [1, 2, 3, 4, 5, 6],
+        1: [1.1, 2, 3, 4, 5],
+        2: [1, 2, 3.2, 4, 5, 6],
         3: [1, 2, 3, 4],
         4: [1, 2, 3, 4, 5, 6, 7],
-    }
-    displayRatings(rates);
+    };
+    displayRatings(ratings);
+    colorTiles();
 })();
 
+function colorTiles() {
+    const colors = [
+        "#ffb3b3",
+        "#ffb8aa",
+        "#ffbfa2",
+        "#ffc69a",
+        "#ffcf95",
+        "#f9d992",
+        "#eee394",
+        "#e0ec99",
+        "#cff6a4",
+        "#bbffb3",
+    ];
+
+    const ratings = document.querySelectorAll('td');
+    console.log(ratings);
+
+    for (let rate of ratings) {
+        if (!rate.classList.contains("index")) {
+            let value = parseInt(rate.textContent) - 1;
+            rate.style.backgroundColor = colors[value];
+        }
+    }
+}
